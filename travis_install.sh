0fd5eb80ae25690c78530a02e9b5e58a78d7c4f4
Matthew Brett <matthew.brett@gmail.com>
2014-11-23 16:29:11 -0800

    Refactor building recipes

--- travis_install.sh
+++ travis_install.sh
@@ -1,95 +1,44 @@
 # Travis install phase
+# vim: let g:is_bash=1
 PIPW="pip wheel -w $WHEELHOUSE"
-PIPI="pip install wheel -f http://travis-wheels.scikit-image.org"
+PIPI="pip install -f http://travis-wheels.scikit-image.org"
+PIPWI="pip install -f $WHEELHOUSE"
+APT_INSTALL="sudo apt-get install"
+BLAS_LAPACK_DEBS="libblas-dev liblapack-dev libatlas3gf-base"
 
-
-function build_cython {
-    $PIPW cython
-    pip install -f $WHEELHOUSE cython
+function build_install {
+    $PIPW $1
+    $PIPWI $1
 }
 
-function build_numpy {
-    sudo apt-get install libblas-dev liblapack-dev libatlas3gf-base gfortran
-    $PIPI cython
-    $PIPW numpy==$NUMPY_VERSION
-    pip install -f $WHEELHOUSE numpy==$NUMPY_VERSION
-}
+$PIPI wheel
 
-function build_matplotlib {
-    sudo apt-get install libpng-dev libfreetype6-dev
-    $PIPI numpy
-    if [[ $TRAVIS_PYTHON_VERSION == 3.2 ]]; then
-        $PIPW matplotlib==1.3.1
-    else
-        $PIPW matplotlib
+for pkg_spec in $TO_BUILD; do
+    IFS="=<>" read -ra pkg_name <<< "$pkg_spec"
+    if [[ $pkg_name =~ ^(numpy|scipy)$ ]]; then
+        $APT_INSTALL $BLAS_LAPACK_DEBS gfortran
+        build_install cython
     fi
-}
-
-
-function build_scipy {
-    sudo apt-get install libblas-dev liblapack-dev libatlas3gf-base gfortran
-    $PIPI cython numpy
-    # scipy needs -v flag otherwise travis times out for lack of output
-    $PIPW -v scipy
-}
-
-function build_pillow {
-    sudo apt-get install libtiff4-dev libwebp-dev
-    $PIPW pillow
-}
-
-
-function build_networkx {
-    $PIPW networkx
-}
-
-function build_tifffile {
-    sudo apt-get install libtiff4-dev libwebp-dev
-    $PIPW tifffile
-}
-
-function build_h5py {
-    sudo apt-get install libhdf5-serial-dev
-    $PIPI cython numpy
-    $PIPW h5py
-}
-
-function build_cvxopt {
-    sudo apt-get install libblas-dev liblapack-dev libatlas3gf-base
-    $PIPW cvxopt
-}
-
-function build_six {
-    $PIPW six
-}
-
-if [[ $TO_BUILD == *cython* ]]; then
-    build_cython
-fi
-if [[ $TO_BUILD == *numpy* ]]; then
-    build_numpy
-fi
-if [[ $TO_BUILD == *scipy* ]]; then
-    build_scipy
-fi
-if [[ $TO_BUILD == *matplotlib* ]]; then
-    build_matplotlib
-fi
-if [[ $TO_BUILD == *pillow* ]]; then
-    build_pillow
-fi
-if [[ $TO_BUILD == *networkx* ]]; then
-    build_networkx
-fi
-if [[ $TO_BUILD == *tifffile* ]]; then
-    build_tifffile
-fi
-if [[ $TO_BUILD == *h5py* ]]; then
-    build_h5py
-fi
-if [[ $TO_BUILD == *cvxopt* ]]; then
-    build_cvxopt
-fi
-if [[ $TO_BUILD == *six* ]]; then
-    build_six
-fi
+    if [[ $pkg_name =~ ^(scipy|matplotlib|pillow|h5py)$ ]]; then
+        if [ -n "$NUMPY_VERSION" ]; then
+            build_install numpy==$NUMPY_VERSION
+        else
+            build_install numpy
+        fi
+    fi
+    if [[ $pkg_name == matplotlib ]]; then
+        $APT_INSTALL libpng-dev libfreetype6-dev
+    elif [[ $pkg_name == pillow ]]; then
+        $APT_INSTALL libtiff4-dev libwebp-dev
+    elif [[ $pkg_name == h5py ]]; then
+        $APT_INSTALL libhdf5-serial-dev
+    elif [[ $pkg_name == cvxopt ]]; then
+        $APT_INSTALL $BLAS_LAPACK_DEBS
+    fi
+    unset pip_extra
+    if [[ $pkg_name == scipy ]]; then
+        # scipy needs -v flag otherwise travis times out for lack of output
+        pip_extra="-v"
+    fi
+    $PIPW $pip_extra $pkg_spec
+done
