e976b39d7d7d35ba8a916ee3a860b86595eaeaa6
Matthew Brett <matthew.brett@gmail.com>
2014-11-26 23:21:39 -0800

    Add scikit-learn wheel

--- travis_install.sh
+++ travis_install.sh
@@ -6,7 +6,9 @@ APT_INSTALL="sudo apt-get install"
 BLAS_LAPACK_DEBS="libblas-dev liblapack-dev libatlas3gf-base"
 PYVER=$TRAVIS_PYTHON_VERSION
 # Packages known to need numpy
-NEEDS_NUMPY="scipy matplotlib pillow h5py"
+NEEDS_NUMPY="scipy matplotlib pillow h5py scikit-learn"
+# Packages known to need scipy
+NEEDS_SCIPY="scikit-learn"
 
 # Install packages we need to build wheels
 $PIPI wheel $PRE_BUILD
@@ -36,6 +38,9 @@ for pkg_spec in $TO_BUILD; do
             $PIPI numpy
         fi
     fi
+    if name_in $pkg_name $NEEDS_SCIPY; then
+        $PIPI scipy
+    fi
     if [[ $pkg_name == matplotlib ]]; then
         $APT_INSTALL libpng-dev libfreetype6-dev
         # Python 3.2 only compiles up to 1.3.1
@@ -46,7 +51,7 @@ for pkg_spec in $TO_BUILD; do
         $APT_INSTALL libtiff4-dev libwebp-dev
     elif [[ $pkg_name == h5py ]]; then
         $APT_INSTALL libhdf5-serial-dev
-    elif [[ $pkg_name == cvxopt ]]; then
+    elif name_in $pkg_name cvxopt scikit-learn; then
         $APT_INSTALL $BLAS_LAPACK_DEBS
     fi
     # scipy needs -v flag otherwise travis times out for lack of output
