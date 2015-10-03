c1b3abed7e827be9d29369b9121ff75324b2ef6e
Steven Silvester <steven.silvester@ieee.org>
2015-10-02 21:08:19 -0500

    Try an apt-get update

--- travis_install.py
+++ travis_install.py
@@ -93,6 +93,7 @@ for pkg_spec in ENV['TO_BUILD'].split():
         run('sudo ln -s /usr/lib/x86_64-linux-gnu/libjpeg.so /usr/lib/')
 
     elif pkg_name_lc == 'pyside':
+        run('sudo apt-get update')
         run('sudo apt-get install build-essential git cmake libqt4-dev')
         run('sudo apt-get install libphonon-dev python2.7-dev libxml2-dev')
         run('sudo apt-get install libxslt1-dev qtmobility-dev')
