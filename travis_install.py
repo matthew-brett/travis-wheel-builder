878a0a27d58e4745047c32a3050cd1565b922792
Matthew Brett <matthew.brett@gmail.com>
2016-03-12 19:38:15 -0800

    Try refactoring the 'no-binary' pip wheel flag

--- travis_install.py
+++ travis_install.py
@@ -36,10 +36,24 @@ def pipi(*args):
     run('pip install {0} {1}'.format(WHEEL_SITE_ARGS, ' '.join(args)))
 
 
-def pipw(*args):
-    """Create a wheel for a package in the WHEELHOUSE"""
-    run('pip wheel -w {0} {1} --no-binary {2} {3}'.format(
-        ENV['WHEELHOUSE'], WHEEL_SITE_ARGS, ','.join(args), ' '.join(args)))
+def pipw(names, specs, extra_args):
+    """Create a wheel for package(s) with `names` and specs `specs`
+    """
+    if not isinstance(names, (list, tuple)):
+        names = [names]
+    if not isinstance(specs, (list, tuple)):
+        specs = [specs]
+    run('pip wheel -w {wheelhouse} {all_args} '
+        '--no-binary {pkg_names} {specs}'.format(
+            wheelhouse=ENV['WHEELHOUSE'],
+            all_args='{0} {1}'.format(WHEEL_SITE_ARGS, extra_args),
+            pkg_names=','.join(names),
+            specs=' '.join(specs)))
+
+
+def spec_to_name(pkg_spec):
+    pkg_name = re.split('[\[ =<>!,]*', pkg_spec)[0]
+    return pkg_name.lower()
 
 
 # Install the packages we need to build wheels
@@ -54,8 +68,7 @@ if not sources:
 for pkg_spec, source in zip(pkg_specs, sources):
     # Get package name from package spec
     # e.g. "matplotlib" from "matplotlib==1.3.1"
-    pkg_name = re.split('[\[ =<>!,]*', pkg_spec)[0]
-    pkg_name_lc = pkg_name.lower()
+    pkg_name = spec_to_name(pkg_spec)
 
     if pkg_name in 'numpy scipy'.split():
         apt_install(BLAS_LAPACK_DEBS, 'gfortran')
@@ -93,26 +106,26 @@ for pkg_spec, source in zip(pkg_specs, sources):
     elif pkg_name in 'cvxopt scikit-learn'.split():
         apt_install(BLAS_LAPACK_DEBS)
 
-    elif pkg_name_lc == 'simpleitk':
+    elif pkg_name == 'simpleitk':
         apt_install('cmake')
 
-    elif pkg_name_lc == 'scikit-image':
+    elif pkg_name == 'scikit-image':
         pipi('six')
 
-    elif pkg_name_lc == 'imread':
+    elif pkg_name == 'imread':
         apt_install('libtiff4-dev libwebp-dev libpng12-dev xcftools')
 
-    elif pkg_name_lc == 'pil':
+    elif pkg_name == 'pil':
         run('sudo apt-get build-dep python-imaging')
         run('sudo apt-get install python-dev')
         run('sudo ln -s /usr/lib/x86_64-linux-gnu/libfreetype.so /usr/lib/')
         run('sudo ln -s /usr/lib/x86_64-linux-gnu/libz.so /usr/lib/')
         run('sudo ln -s /usr/lib/x86_64-linux-gnu/libjpeg.so /usr/lib/')
 
-    elif pkg_name_lc == 'pymvpa2':
+    elif pkg_name == 'pymvpa2':
         run('sudo apt-get install swig')
 
-    elif pkg_name_lc == 'pyside':
+    elif pkg_name == 'pyside':
         apt_install('build-essential git cmake libqt4-dev libphonon-dev')
         apt_install('python2.7-dev libxml2-dev libxslt1-dev qtmobility-dev')
         run('wget https://pypi.python.org/packages/source/P/PySide/PySide-1.2.2.tar.gz')
@@ -126,10 +139,10 @@ for pkg_spec, source in zip(pkg_specs, sources):
         source = pkg_spec
 
     # scipy needs -v flag otherwise travis times out for lack of output
-    if pkg_name_lc == 'scipy':
-        pipw('-v', source)
+    if pkg_name == 'scipy':
+        pipw(pkg_name, source, '-v')
 
-    elif pkg_name_lc == 'simpleitk':
+    elif pkg_name == 'simpleitk':
         link = 'http://sourceforge.net/projects/simpleitk/files/SimpleITK/0.8.0/Python/SimpleITK-0.8.0-cp%s-%s-linux_x86_64.whl'
         ver = PYVER.replace('.', '')
         if ver in ['26', '27']:
@@ -140,12 +153,13 @@ for pkg_spec, source in zip(pkg_specs, sources):
             continue
         run('wget %s -P %s' % (link, ENV['WHEELHOUSE']))
 
-    elif pkg_name_lc == 'pil':
-        pipw('--allow-external', 'PIL', '--allow-unverified', 'PIL', 'pil')
+    elif pkg_name == 'pil':
+        pipw(pkg_name, source,
+            '--allow-external PIL --allow-unverified PIL')
 
-    elif pkg_name_lc == 'pyside':
+    elif pkg_name == 'pyside':
         # patch for Py35 compatibility
         run("cd PySide-1.2.2 && sed -i 's/subprocess.mswindows/False/g' popenasync.py")
         run('cd PySide-1.2.2 && python setup.py bdist_wheel --qmake=/usr/bin/qmake-qt4 -d %s' % ENV['WHEELHOUSE'])
     else:
-        pipw(source)
+        pipw(pkg_name, source)
