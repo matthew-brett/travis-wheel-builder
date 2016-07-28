c831ca116f85802f260ddb4055f214ef09df9d0c
Matthew Brett <matthew.brett@gmail.com>
2016-07-28 12:39:20 -0700

    Try building pytables

--- travis_install.py
+++ travis_install.py
@@ -14,7 +14,7 @@ PYVER = ENV['TRAVIS_PYTHON_VERSION']
 # Packages known to need numpy
 NEEDS_NUMPY = ("scipy matplotlib pillow h5py scikit-learn astropy "
                "scikit-image pandas dipy nipy statsmodels pymvpa2 "
-               "PyWavelets ")
+               "PyWavelets pytables")
 # Packages known to need scipy
 NEEDS_SCIPY = "scikit-learn statsmodels dipy"
 # Packages known to need Cython
